76cacdbe612b3929bcdaa20319e8c8b0a58e6efe
null <mywei1989@gmail.com>
2016-03-01 23:14:42 +0800

    修复页码展示bug

--- assets/blog/js/Pagination.js
+++ assets/blog/js/Pagination.js
@@ -41,40 +41,41 @@ class Pagination extends React.Component{
     let pageHtml=[];
     if(this.props.pageIndex>1){
       if(this.props.pageIndex-1===1){
-        pageHtml.push(<a className="extend prev" rel="prev" href={this.props.prefix+"/"} key="«">«</a>);
+        //pageHtml.push(<a className="extend prev" rel="prev" href={this.props.prefix+"/"} key="«">«</a>);
+        pageHtml.push(<Link className="extend prev" rel="prev" to={this.props.prefix+"/"} key="«">«</Link>);
       }else{
-        pageHtml.push(<a className="extend prev" rel="prev" href={this.props.prefix+"/page/"+(this.props.pageIndex-1)+"/"} key="«">«</a>);
+        pageHtml.push(<Link className="extend prev" rel="prev" to={this.props.prefix+"/page/"+(this.props.pageIndex-1)+"/"} key="«">«</Link>);
       }
     }
     if(this.props.pageIndex===2){
-      pageHtml.push(<a className="page-number" href={this.props.prefix+"/"} key="1">1</a>);
+      pageHtml.push(<Link className="page-number" to={this.props.prefix+"/"} key="1">1</Link>);
     }else if(this.props.pageIndex===3){
-      pageHtml.push(<a className="page-number" href={this.props.prefix+"/"} key="1">1</a>);
-      pageHtml.push(<a className="page-number" href={this.props.prefix+"/page/2/"} key="2">2</a>);
+      pageHtml.push(<Link className="page-number" to={this.props.prefix+"/"} key="1">1</Link>);
+      pageHtml.push(<Link className="page-number" to={this.props.prefix+"/page/2/"} key="2">2</Link>);
     }else if(this.props.pageIndex===4){
-      pageHtml.push(<a className="page-number" href="/" key="1">1</a>);
-      pageHtml.push(<a className="page-number" href={this.props.prefix+"/page/2/" } key="2">2</a>);
-      pageHtml.push(<a className="page-number" href={this.props.prefix+"/page/3/" } key="3">3</a>);
-    }else if(this.props.pageIndex===5){
-      pageHtml.push(<a className="page-number" href="/" key="1">1</a>);
+      pageHtml.push(<Link className="page-number" to="/" key="1">1</Link>);
+      pageHtml.push(<Link className="page-number" to={this.props.prefix+"/page/2/" } key="2">2</Link>);
+      pageHtml.push(<Link className="page-number" to={this.props.prefix+"/page/3/" } key="3">3</Link>);
+    }else if(this.props.pageIndex>=5){
+      pageHtml.push(<Link className="page-number" to="/" key="1">1</Link>);
       pageHtml.push(<span className="space" key="...">…</span>);
       for(let i=this.props.pageIndex-2;i<this.props.pageIndex;i++){
-        pageHtml.push(<a className="page-number" href={this.props.prefix+"/page/"+i+"/" } key={i}>{i}</a>);
+        pageHtml.push(<Link className="page-number" to={this.props.prefix+"/page/"+i+"/" } key={i}>{i}</Link>);
       } 
     }
     pageHtml.push(<span className="page-number current" key={this.props.pageIndex} key={this.props.pageIndex}>{this.props.pageIndex}</span>);
     if(this.props.pageCount - this.props.pageIndex >= 4){
-      pageHtml.push(<a className="page-number" href={this.props.prefix+"/page/"+(this.props.pageIndex+1)+"/"} key={this.props.pageIndex+1} key={this.props.pageIndex+1} >{this.props.pageIndex+1}</a>);
-      pageHtml.push(<a className="page-number" href={this.props.prefix+"/page/"+(this.props.pageIndex+2)+"/"} key={this.props.pageIndex+2} key={this.props.pageIndex+2} >{this.props.pageIndex+2}</a>);
+      pageHtml.push(<Link className="page-number" to={this.props.prefix+"/page/"+(this.props.pageIndex+1)+"/"} key={this.props.pageIndex+1} key={this.props.pageIndex+1} >{this.props.pageIndex+1}</Link>);
+      pageHtml.push(<Link className="page-number" to={this.props.prefix+"/page/"+(this.props.pageIndex+2)+"/"} key={this.props.pageIndex+2} key={this.props.pageIndex+2} >{this.props.pageIndex+2}</Link>);
       pageHtml.push(<span className="space" key="...">…</span>);
-      pageHtml.push(<a className="page-number" href={this.props.prefix+"/page/"+this.props.pageCount+"/"} key={this.props.pageCount}>{this.props.pageCount}</a>);
+      pageHtml.push(<Link className="page-number" to={this.props.prefix+"/page/"+this.props.pageCount+"/"} key={this.props.pageCount}>{this.props.pageCount}</Link>);
     }else{
       for(let i=this.props.pageIndex+1;i<=this.props.pageCount;i++){
-        pageHtml.push(<a className="page-number" href={this.props.prefix+"/page/"+i+"/"} key={i}>{i}</a>);
+        pageHtml.push(<Link className="page-number" to={this.props.prefix+"/page/"+i+"/"} key={i}>{i}</Link>);
       }
     }
     if(this.props.pageIndex!==this.props.pageCount){
-      pageHtml.push(<a className="extend next" rel="next" href={this.props.prefix+"/page/"+(this.props.pageIndex+1)+"/"} key="»">»</a>);
+      pageHtml.push(<Link className="extend next" rel="next" to={this.props.prefix+"/page/"+(this.props.pageIndex+1)+"/"} key="»">»</Link>);
     }
 
 
